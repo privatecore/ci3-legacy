0cf2319d9625011c551873dac02fc2c12807184b
privatecore <privatecorepp@gmail.com>
2024-06-21 11:17:34 +0200

    Update autoload configuration -> process everythin application need to work properly

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -1,5 +1,4 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------
@@ -45,9 +44,8 @@ $autoload['packages'] = array();
 | -------------------------------------------------------------------
 |  Auto-load Libraries
 | -------------------------------------------------------------------
-| These are the classes located in system/libraries/ or your
-| application/libraries/ directory, with the addition of the
-| 'database' library, which is somewhat of a special case.
+| These are the classes located in the system/libraries folder
+| or in your application/libraries folder.
 |
 | Prototype:
 |
@@ -58,26 +56,19 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array();
+$autoload['libraries'] = array('database', 'session', 'form_validation', 'pagination', 'email', 'cart', 'acl', 'user_agent');
 
 /*
 | -------------------------------------------------------------------
 |  Auto-load Drivers
 | -------------------------------------------------------------------
-| These classes are located in system/libraries/ or in your
-| application/libraries/ directory, but are also placed inside their
-| own subdirectory and they extend the CI_Driver_Library class. They
+| These classes are located in the system/libraries folder or in your
+| application/libraries folder within their own subdirectory. They
 | offer multiple interchangeable driver options.
 |
 | Prototype:
 |
-|	$autoload['drivers'] = array('cache');
-|
-| You can also supply an alternative property name to be assigned in
-| the controller:
-|
-|	$autoload['drivers'] = array('cache' => 'cch');
-|
+|	$autoload['drivers'] = array('session', 'cache');
 */
 $autoload['drivers'] = array();
 
@@ -89,7 +80,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array();
+$autoload['helper'] = array('core', 'date', 'file', 'form', 'html', 'language', 'output', 'path', 'security', 'string', 'text', 'url');
 
 /*
 | -------------------------------------------------------------------
@@ -103,7 +94,7 @@ $autoload['helper'] = array();
 | config files.  Otherwise, leave it blank.
 |
 */
-$autoload['config'] = array();
+$autoload['config'] = array('core', 'timezones');
 
 /*
 | -------------------------------------------------------------------
@@ -117,7 +108,7 @@ $autoload['config'] = array();
 | "codeigniter_lang.php" would be referenced as array('codeigniter');
 |
 */
-$autoload['language'] = array();
+$autoload['language'] = array('core');
 
 /*
 | -------------------------------------------------------------------
@@ -132,4 +123,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array();
+$autoload['model'] = array('setting_model');

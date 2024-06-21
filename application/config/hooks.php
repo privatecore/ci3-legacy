aa3bf61b22bf1acc1bbf7266447f01cfcce3389b
privatecore <privatecorepp@gmail.com>
2024-06-21 11:28:25 +0200

    Add hooks configuration example to start with -> more documentation can be found on official website

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -1,13 +1,16 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 /*
-| -------------------------------------------------------------------------
+|--------------------------------------------------------------------------
 | Hooks
-| -------------------------------------------------------------------------
+|--------------------------------------------------------------------------
+|
 | This file lets you define "hooks" to extend CI without hacking the core
 | files.  Please see the user guide for info:
 |
 |	https://codeigniter.com/userguide3/general/hooks.html
 |
 */
+
+$hook['pre_system'] = array('function' => 'trim_post', 'filename' => 'input.php', 'filepath' => 'hooks');
+$hook['display_override'] = array('function' => 'compress_html', 'filename' => 'output.php', 'filepath' => 'hooks');

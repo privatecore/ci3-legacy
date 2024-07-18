a6f4558a422c4bbd5a2accb02e9746cf16e7da08
privatecore <privatecorepp@gmail.com>
2024-07-18 22:03:04 +0300

    Add AJAX placeholder for public related controllers

---
+++ application/controllers/public/Ajax.php
@@ -0,0 +1,20 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Ajax class
+ *
+ * All AJAX requests should go in here. CSRF protection has been disabled
+ * for this controller in the config file. IMPORTANT! For retrieving data
+ * only.
+ */
+class Ajax extends Ajax_Controller {
+
+	/**
+	 * Constructor
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+}
